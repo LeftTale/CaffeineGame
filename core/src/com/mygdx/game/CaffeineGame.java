07a28b2956e06337927a46f1d795d400f5ad65ff
Caolan <notretroid@outlook.com>
2021-09-04 19:56:12 +0100

    commit1

--- core/src/com/mygdx/game/CaffeineGame.java
+++ core/src/com/mygdx/game/CaffeineGame.java
@@ -1,31 +1,64 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class CaffeineGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture milkJugImage;
+	private OrthographicCamera camera;
+	private Rectangle milkJug;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		milkJugImage = new Texture("tempjug.png");
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 1000,500);
+
+		milkJug = new Rectangle();
+		milkJug.x = 600;
+		milkJug.y = 90;
+		milkJug.width = 64;
+		milkJug.height = 64;
+
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(.5f, 0.2f, 0.9f, 1);
+		camera.update();
+
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(milkJugImage,
+				milkJug.x,
+				milkJug.y,
+				milkJug.x,
+				milkJug.y,
+				milkJug.width,
+				milkJug.height,
+				1f,
+				1f,
+				0f,
+				0,
+				0,
+				64,
+				64,
+				false,
+				false
+				);
 		batch.end();
+
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		milkJugImage.dispose();
 	}
 }
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.mygdx.game.CaffeineGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Caffeine 100%";
+		config.height = 500;
+		config.width = 1000;
 		new LwjglApplication(new CaffeineGame(), config);
 	}
 }
